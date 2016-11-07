a45cd1470dcfa2cef2427c8df5c3707868dbbb6c
Saatvik Ramisetty <vishnu.saatvik@gmail.com>
2016-11-06 23:54:20 -0500

    Final commit.
    Added my code to the submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Return list that contains all the functions like setting, getting matrix and inverse...
+## List is eventually used as the input to cacheSolve
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix <- function(x = matrix())
+{
+  inverse = NULL
+  setMat = function(y)
+  {
+    x <<- y
+    inverse <<- NULL
+  }
+  
+  getMat = function() x
+  setInv = function(inv) inverse <<- inv
+  getInv = function() inverse
+  list(setMat = setMat,getMat = getMat,setInv=setInv,getInv=getInv)
+  
 }
 
 
@@ -12,4 +27,35 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inverse = x$getInv()
+  
+  if(!is.null(inverse))
+  {
+    message("Already computed. Getting the cached Data...")
+    return(inverse)
+  }
+  
+  matrixdata = x$getMat()
+  inverse = solve(matrixdata,...)
+  
+  x$setInv(inverse)
+  
+  return(inverse)
+  
+}
+
+## This test function can be used to check the reduction in computation time due to caching..
+test = function(mat){
+  
+  temp = makeCacheMatrix(mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
 }
